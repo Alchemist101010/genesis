f454a7a858edcb5ba77f417c5341fb1b18720b76
Alchemist101010 <44228781+Alchemist101010@users.noreply.github.com>
2018-12-16 15:23:27 +0530

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -8,14 +8,14 @@ X = dataset[:, 0:8]
 Y = dataset[:, 8]
 
 model = Sequential()
-model.add(Dense, (12, input_dim == 8, activation =='relu'))
+model.add(Dense, (12, input_dim=8, activation=relu'))
 model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 # Compile model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 # Fit the model
-model.fit(X, Y, epochs=150, batch_size=10)
+model.fit(X, Y, epochs=10, batch_size=10)
 # evaluate the model
 scores = model.evaluate(X, Y)
-print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+print(f"The metric is {model.metrics_names[1]}\n And its score is {scores[1]*100}")
